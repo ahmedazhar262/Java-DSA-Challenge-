e87b85f1fa3e4aa65d3e148729b2f8252fe2f2e0
ahmedazhar262 <95431771+ahmedazhar262@users.noreply.github.com>
2023-09-19 22:25:30 +0530

    Update Minimum and Maximum Array.java

--- Easy/Minimum and Maximum Array.java
+++ Easy/Minimum and Maximum Array.java
@@ -19,7 +19,9 @@ public class MaxandMinArray{
             if(arr[i]>Max)
               Max=arr[i];
         }
+        System.out.println("Minimum Array" + Min);
+        System.out.println("Maximum Array" + Max);
 
     }
-    System.out.println("Minimum Array" + Min);
-    System.out.println("Maximum Array" + Max);
+}
+    
